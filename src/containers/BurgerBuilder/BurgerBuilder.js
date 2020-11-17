c666b18329ca453893fd66b4a0b9a08daba67e24
Latch Jackn <latch.jack@gmail.com>
2020-11-17 14:25:02 +0000

    updated

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -106,8 +106,12 @@ class BurgerBuilder extends Component {
     this.setState({ purchasing: true })
   }
 
-  purchaseCancelHander = () => {
+  purchaseCancelHandler = () => {
     this.setState({ purchasing: false })
+  }
+
+  purchaseContinueHandler = () => {
+    alert('You continue!');
   }
 
   render() {
@@ -119,8 +123,13 @@ class BurgerBuilder extends Component {
     };
     return (
       <Aux>
-        <Modal show={this.state.purchasing} modalClosed={this.purchaseCancelHander}>
-          <OrderSummary ingredients={this.state.ingredients} />
+        <Modal show={this.state.purchasing} modalClosed={this.purchaseCancelHandler}>
+          <OrderSummary
+            ingredients={this.state.ingredients}
+            price={this.state.totalPrice}
+            purchaseCancelled={this.purchaseCancelHandler}
+            purchaseContinued={this.purchaseContinueHandler}
+          />
         </Modal>
         <Burger ingredients={this.state.ingredients} />
         <BuildControls
