bbada17ea35a2c36e7c56e297b3b7689bd331605
Latch Jackn <latch.jack@gmail.com>
2020-11-18 12:48:48 +0000

    before merge

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -5,11 +5,28 @@ import Toolbar from '../Navigation/Toolbar/Toolbar';
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
 class Layout extends Component {
+  state = {
+    showSideDrawer: true
+  }
+
+  sideDrawerClosedHandler = () => {
+    this.setState({ showSideDrawer: false });
+  }
+
+  sideDrawerTogglerHander = () => {
+    this.setState((prevState) => {
+      return { showSideDrawer: !prevState.showSideDrawer }
+    });
+  }
+
   render() {
     return (
       <Aux>
-        <Toolbar />
-        <SideDrawer />
+        <Toolbar drawerToggleClicked={this.sideDrawerTogglerHander} />
+        <SideDrawer
+          open={this.state.showSideDrawer}
+          closed={this.sideDrawerClosedHandler}
+        />
         <main className={classes.Content}>
           {this.props.children}
         </main>
--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -4,12 +4,18 @@ import NavigationItems from '../NavigationItems/NavigationItems';
 import classes from './SideDrawer.module.css';
 import Backdrop from '../../UI/Backdrop/Backdrop';
 import Aux from '../../../hoc/Aux';
+
 const sideDrawer = (props) => {
+  let attachedClasses = [classes.SideDrawer, classes.Close];
+
+  if (props.open) {
+    attachedClasses = [classes.SideDrawer, classes.Open];
+  }
 
   return (
     <Aux>
-      <Backdrop show />
-      <div className={classes.SideDrawer}>
+      <Backdrop show={props.open} clicked={props.closed} />
+      <div className={attachedClasses.join('')}>
         <div className={classes.Logo}>
           <Logo />
         </div>
