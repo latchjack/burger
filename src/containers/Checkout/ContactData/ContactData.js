2d6cb3b617b28962ae97df20928fafe6c16bc9af
Latch Jackn <latch.jack@gmail.com>
2020-11-26 17:02:11 +0000

    eod

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import axios from '../../../axios-orders';
 import Button from '../../../components/UI/Button/Button';
 import Spinner from '../../../components/UI/Spinner/Spinner';
+import Input from '../../../components/UI/Input/Input';
 import classes from './ContactData.module.css';
 
 class ContactData extends Component {
@@ -55,10 +56,10 @@ class ContactData extends Component {
   render() {
     let form = (
       <form>
-        <input className={classes.Input} type="text" name="name" placeholder="Your Name" />
-        <input className={classes.Input} type="email" name="email" placeholder="Your Email" />
-        <input className={classes.Input} type="text" name="street" placeholder="Street Name" />
-        <input className={classes.Input} type="text" name="postal" placeholder="Postal Code" />
+        <Input type="text" name="name" placeholder="Your Name" />
+        <Input type="email" name="email" placeholder="Your Email" />
+        <Input type="text" name="street" placeholder="Street Name" />
+        <Input type="text" name="postal" placeholder="Postal Code" />
         <Button btnType="Success" clicked={this.orderHandler}>ORDER</Button>
       </form>
     );
