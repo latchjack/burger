618fefc30c5bcb9de742572ef5740d4ede85a250
Latch Jackn <latch.jack@gmail.com>
2020-12-03 09:34:50 +0000

    added ingredients state to redux

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-
+import { connect } from 'react-redux';
 import Aux from '../../hoc/Aux/Aux';
 import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
@@ -8,6 +8,7 @@ import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
 import Spinner from '../../components/UI/Spinner/Spinner';
 import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
 import axios from '../../axios-orders';
+import * as actionTypes from '../../store/actions';
 
 
 const INGREDIENT_PRICES = {
@@ -35,7 +36,6 @@ class BurgerBuilder extends Component {
   | =============================================
   */
   state = {
-    ingredients: null,
     totalPrice: 4,
     purchasable: false,
     purchasing: false,
@@ -172,7 +172,7 @@ class BurgerBuilder extends Component {
 
   render () {
     const disabledInfo = {
-      ...this.state.ingredients
+      ...this.props.ings
     };
     for ( let key in disabledInfo ) {
       disabledInfo[key] = disabledInfo[key] <= 0
@@ -206,13 +206,13 @@ class BurgerBuilder extends Component {
     */
     let burger = this.state.error ? <p>Ingredients can't be loaded!</p> : <Spinner />;
 
-    if(this.state.ingredients) {
+    if(this.props.ings) {
       burger = (
         <Aux>
-          <Burger ingredients={this.state.ingredients} />
+          <Burger ingredients={this.props.ings} />
           <BuildControls
-            ingredientAdded={this.addIngredientHandler}
-            ingredientRemoved={this.removeIngredientHandler}
+            ingredientAdded={this.props.onIngredientAdded}
+            ingredientRemoved={this.props.onIngredientRemoved}
             disabled={disabledInfo}
             purchasable={this.state.purchasable}
             ordered={this.purchaseHandler}
@@ -222,7 +222,7 @@ class BurgerBuilder extends Component {
       );
       orderSummary = (
         <OrderSummary 
-          ingredients={this.state.ingredients}
+          ingredients={this.props.ings}
           price={this.state.totalPrice}
           purchaseCancelled={this.purchaseCancelHandler}
           purchaseContinued={this.purchaseContinueHandler}
@@ -245,6 +245,22 @@ class BurgerBuilder extends Component {
   }
 }
 
+const mapStateToProps = state => {
+  return {
+    ings: state.ingredients
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    onIngredientAdded: (ingName) => dispatch({type: actionTypes.ADD_INGREDIENT, ingredientName: ingName}),
+    onIngredientRemoved: (ingName) => dispatch({type: actionTypes.REMOVE_INGREDIENT, ingredientName: ingName})
+  }
+}
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(withErrorHandler(BurgerBuilder, axios));
+
 /*
 |==========================================================
 | For the withErrorHandler HOC to receive the information of
@@ -258,5 +274,3 @@ class BurgerBuilder extends Component {
 | interceptors. Once an error has been issued, it will respond.
 |==========================================================
  */
-
-export default withErrorHandler(BurgerBuilder, axios);
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -15,7 +15,10 @@ const initialState = {
 | Below we're creating a deep clone of the ingredients object that is in 
 | state. First we clone the ingredients - ...ingredients - and then we go
 | a level down and clone again using ...state.ingredients.
-| We then use an es6 feature, 
+| We then use an es6 feature, to dynamically override the ingredient. The
+| square brackets will get ingredientName as a payload from the dispatch.
+| We target that particular ingredient with the sqaures brackets and use 
+| it to set the new value of the ingredient with state.ingredients[action.ingredientName] + 1
 |=====================================
 */
 
@@ -26,12 +29,16 @@ const reducer = (state = initialState, action) => {
         ...state,
         ingredients: {
           ...state.ingredients,
-          [action.ingredientName]
+          [action.ingredientName]: state.ingredients[action.ingredientName] + 1
         }
       };
     case actionTypes.REMOVE_INGREDIENT:
       return {
-
+        ...state,
+        ingredients: {
+          ...state.ingredients,
+          [action.ingredientName]: state.ingredients[action.ingredientName] - 1
+        }
       };
     default:
       return state;
