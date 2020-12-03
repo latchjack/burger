5fd4db9cb75bba7690893ecf3ff6f77ef08bd197
Latch Jackn <latch.jack@gmail.com>
2020-12-03 09:49:42 +0000

    added price to redux state

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -10,6 +10,13 @@ const initialState = {
   totalPrice: 4
 };
 
+const INGREDIENT_PRICES = {
+  salad: 0.5,
+  cheese: 0.4,
+  meat: 1.3,
+  bacon: 0.7
+};
+
 /*
 |=====================================
 | Below we're creating a deep clone of the ingredients object that is in 
@@ -30,7 +37,8 @@ const reducer = (state = initialState, action) => {
         ingredients: {
           ...state.ingredients,
           [action.ingredientName]: state.ingredients[action.ingredientName] + 1
-        }
+        },
+        totalPrice: state.totalPrice + INGREDIENT_PRICES[action.ingredientName]
       };
     case actionTypes.REMOVE_INGREDIENT:
       return {
@@ -38,7 +46,8 @@ const reducer = (state = initialState, action) => {
         ingredients: {
           ...state.ingredients,
           [action.ingredientName]: state.ingredients[action.ingredientName] - 1
-        }
+        },
+        totalPrice: state.totalPrice - INGREDIENT_PRICES[action.ingredientName]
       };
     default:
       return state;
@@ -46,3 +55,9 @@ const reducer = (state = initialState, action) => {
 }
 
 export default reducer;
+
+/*
+| To update the price we could either add more actions for it in our actions
+| file (INCREASE_PRICE & DECREASE_PRICE) or the route we choose here, where
+| we update it alongside our ingredient in the same actions.
+*/
