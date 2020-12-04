a569959f315aca8dce5e8b86131d5c3885d189fa
Latch Jackn <latch.jack@gmail.com>
2020-12-04 14:29:11 +0000

    update

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -6,8 +6,8 @@ import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
 import Spinner from '../../components/UI/Spinner/Spinner';
-import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
 import axios from '../../axios-orders';
+import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
 import * as burgerBuilderActions from '../../store/actions/index';
 
 class BurgerBuilder extends Component {
@@ -15,32 +15,13 @@ class BurgerBuilder extends Component {
   //     super(props);
   //     this.state = {...}
   // }
-
-  /* ============================================
-  | We want to sum up all the values in the ingredients object,
-  | so that we can control if the burger is able to be purchased,
-  | or not.
-  | Below converts the object into an array, so that we can use
-  | the map and reduce functions on our ingredients list. 
-  | This function will give us the combined amount of ingredients
-  | (sum) at the end, to see if the burger has had ingredients 
-  | added so it is able to be purchased (enabling the order button).
-  | =============================================
-  */
   state = {
-    purchasing: false,
-    loading: false,
-    error: false
+    purchasing: false
   }
 
   componentDidMount() {
-    // axios.get('https://burgerbuilder-7f999.firebaseio.com/ingredients.json')
-    //   .then(response => {
-    //     this.setState({ ingredients: response.data });
-    //   })
-    //   .catch(error => {
-    //     this.setState({ error: true })
-    //   });
+    console.log(this.props);
+    this.props.onInitIngredients();
   }
 
   updatePurchaseState (ingredients) {
@@ -129,7 +110,7 @@ class BurgerBuilder extends Component {
     | returned as true when it is checked in the if statement.
     |======================================================
     */
-    let burger = this.state.error ? <p>Ingredients can't be loaded!</p> : <Spinner />;
+    let burger = this.props.error ? <p>Ingredients can't be loaded!</p> : <Spinner />;
 
     if(this.props.ings) {
       burger = (
@@ -154,10 +135,6 @@ class BurgerBuilder extends Component {
         />
       );
     }
-    if (this.state.loading) {
-      orderSummary = <Spinner />;
-    }
-
     // {salad: true, meat: false, ...}
     return (
       <Aux>
@@ -173,14 +150,16 @@ class BurgerBuilder extends Component {
 const mapStateToProps = state => {
   return {
     ings: state.ingredients,
-    price: state.totalPrice
+    price: state.totalPrice,
+    error: state.error
   }
 }
 
 const mapDispatchToProps = dispatch => {
   return {
     onIngredientAdded: (ingName) => dispatch(burgerBuilderActions.addIngredient(ingName)),
-    onIngredientRemoved: (ingName) => dispatch(burgerBuilderActions.removeIngredient(ingName))
+    onIngredientRemoved: (ingName) => dispatch(burgerBuilderActions.removeIngredient(ingName)),
+    onInitIngredients: () => dispatch(burgerBuilderActions.initIngredients())
   }
 }
 
