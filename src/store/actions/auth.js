99a15a1288e48c02ee362e31ac8725fd09347480
Latch Jackn <latch.jack@gmail.com>
2020-12-08 15:27:36 +0000

    before token

---
+++ src/store/actions/auth.js
@@ -0,0 +1,46 @@
+import axios from 'axios';
+import * as actionTypes from './actionTypes';
+
+export const authStart = () => {
+  return {
+    type: actionTypes.AUTH_START
+  };
+};
+
+export const authSuccess = (authData) => {
+  return {
+    type: actionTypes.AUTH_SUCCESS,
+    authData: authData
+  };
+};
+
+export const authFail = (error) => {
+  return {
+    type: actionTypes.AUTH_FAIL,
+    error: error
+  };
+};
+
+export const auth =(email, password, isSignup) => {
+  return dispatch => {
+    dispatch(authStart());
+    const authData = {
+      email: email,
+      password: password,
+      returnSecureToken: true
+    };
+    let url = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAXW0fhhr5AIz66z_lr4NY2oTRaZ5iG0MY';
+    if (!isSignup) {
+      url = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyAXW0fhhr5AIz66z_lr4NY2oTRaZ5iG0MY';
+    }
+    axios.post(url, authData)
+      .then(response => {
+        console.log(response);
+        dispatch(authSuccess(response.data));
+      })
+      .catch(err => {
+        console.log(err);
+        dispatch(authFail(err));
+      })
+  }
+}
