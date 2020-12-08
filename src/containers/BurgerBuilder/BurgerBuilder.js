8687a84bb7de3c12f377e236047f0f02b33f08f4
Latch Jackn <latch.jack@gmail.com>
2020-12-08 18:38:50 +0000

    fixed redirecting

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -62,6 +62,7 @@ class BurgerBuilder extends Component {
     if (this.props.isAuthenticated) {
       this.setState({purchasing: true});
     } else {
+      this.props.onAuthSetRedirectPath('/checkout');
       this.props.history.push('/auth');
     }
   }
@@ -167,7 +168,8 @@ const mapDispatchToProps = dispatch => {
     onIngredientAdded: (ingName) => dispatch(actions.addIngredient(ingName)),
     onIngredientRemoved: (ingName) => dispatch(actions.removeIngredient(ingName)),
     onInitIngredients: () => dispatch(actions.initIngredients()),
-    onInitPurchase: () => dispatch(actions.purchaseInit())
+    onInitPurchase: () => dispatch(actions.purchaseInit()),
+    onAuthSetRedirectPath: (path) => dispatch(actions.setAuthRedirectPath(path))
   }
 }
 
