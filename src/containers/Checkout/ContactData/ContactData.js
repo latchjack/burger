ffba73903a17bde6f368d93d0c4180ea0e769a9c
Latch Jackn <latch.jack@gmail.com>
2020-12-09 12:13:15 +0000

    outsourced validity check

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -7,7 +7,7 @@ import Input from '../../../components/UI/Input/Input';
 import classes from './ContactData.module.css';
 import withErrorHandler from '../../../hoc/withErrorHandler/withErrorHandler';
 import * as actions from '../../../store/actions/index';
-import { updateObject } from '../../../shared/utility';
+import { updateObject, checkValidity } from '../../../shared/utility';
 
 class ContactData extends Component {
   state = {
@@ -152,32 +152,6 @@ class ContactData extends Component {
     }
     this.props.onOrderBurger(order, this.props.token);
   }
-  
-  checkValidity (value, rules) {
-    let isValid = true;
-    if (!rules) {
-      return true;
-    }
-    if (rules.required) {
-      isValid = value.trim() !== '' && isValid;
-    }
-    if (rules.minLength) {
-      isValid = value.length >= rules.minLength && isValid;
-    }
-    if (rules.maxLength) {
-      isValid = value.length <= rules.maxLength && isValid;
-    }
-    /*
-    |======================================================
-    | By chaining the '&& isValid' condition to the end of 
-    | each rule, it is checking to make sure that all the 
-    | rules pass. Otherwise, without it, even if all the prior
-    | checks fail but the last If check passes it would let
-    | the whole validity check pass.
-    |======================================================
-    */
-    return isValid;
-  }
 
   inputChangedHandler = (event, inputIdentifier) => {
     /*
@@ -210,7 +184,7 @@ class ContactData extends Component {
 
     const updatedFormElement = updateObject(this.state.orderForm[inputIdentifier], {
       value: event.target.value,
-      valid: this.checkValidity(event.target.value, this.state.orderForm[inputIdentifier].validation),
+      valid: checkValidity(event.target.value, this.state.orderForm[inputIdentifier].validation),
       touched: true
     });
     const updatedOrderForm = updateObject(this.state.orderForm, {
