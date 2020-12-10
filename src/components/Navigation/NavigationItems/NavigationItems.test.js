f40e9a6c3c19bcdc1265eebfd2494c2d0a0f6b7d
Latch Jackn <latch.jack@gmail.com>
2020-12-10 11:53:41 +0000

    adding tests

--- src/components/Navigation/NavigationItems/NavigationItems.test.js
+++ src/components/Navigation/NavigationItems/NavigationItems.test.js
@@ -12,4 +12,9 @@ describe('<NavigationItems />', () => {
     const wrapper = shallow(<NavigationItems />);
     expect(wrapper.find(NavigationItem)).toHaveLength(2);
   });
+
+  it('should render three <NavigationItem /> elements if authenticated', () => {
+    const wrapper = shallow(<NavigationItems isAuthenticated />);
+    expect(wrapper.find(NavigationItem)).toHaveLength(3);
+  });
 })
